41a8292e16c110691b4c56b9c909608aa0e00806
SkeTJ <73380956+SkeTJ@users.noreply.github.com>
2021-07-05 18:30:05 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 import socket
 import tkinter as tk
+import kivy #owo
 
 #ngl i think using kivy would be more ni1cer for the ui than tkinter coz it looks more modern, besides,
 #its wat i use for ma fyp too :D -jul sux hehehe omg, its u (only old ppl know this reference)
