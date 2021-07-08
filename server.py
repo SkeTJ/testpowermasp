f6961d96eb0f2ef8447adf7f6536ec5973497b19
ZeeJinx <86944977+ZeeJinx@users.noreply.github.com>
2021-07-08 16:10:35 +0800

    Update server.py

--- server.py
+++ server.py
@@ -3,39 +3,21 @@ import socket
 import tkinter as tk
 import kivy #owo
 import wmi
-import win32con
-import win32service
 
 #ngl i think using kivy would be more ni1cer for the ui than tkinter coz it looks more modern, besides,
 #its wat i use for ma fyp too :D -jul sux hehehe omg, its u (only old ppl know this reference)
 #master programmer, use what you want. imma just do the functions that i was ASSigned
 
+HOST = ""
+PORT = 
+s = socket.socket()
+s.bind((HOST,PORT))
 
-
+def cpu_usage():
+  
 
 def processlist():
-  while True:
-    f = wmi.WMI() #initialize wmi constructor
-    print("pid  Process name") #print header
-    for process in f.Win32_Process(): #iterate through running processes
-      print(f"{process.ProcessId:<10} {process.Name}") #display name and id of processes
-      
+   
 def services():
-  while True:
-    resume = 0
-    accessSCM = win32con.GENERIC_READ
-    accessSrv = win32service.SC_MANAGER_ALL_ACCESS
-
-    #Open Service Control Manager
-    hscm = win32service.OpenSCManager(None, None, accessSCM)
-
-    #Enumerate Service Control Manager DB
-    typeFilter = win32service.SERVICE_WIN32
-    stateFilter = win32service.SERVICE_STATE_ALL
-
-    statuses = win32service.EnumServicesStatus(hscm, typeFilter, stateFilter)
-
-    for (short_name, desc, status) in statuses:
-        print(short_name, desc, status)
-  
+    
   
