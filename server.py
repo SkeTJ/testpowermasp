5499dd4e2f4fef37f05f14905c87c85a7b56c12b
SkeTJ <73380956+SkeTJ@users.noreply.github.com>
2021-07-12 22:20:30 +0800

    Update server.py

--- server.py
+++ server.py
@@ -8,10 +8,24 @@ import wmi
 #its wat i use for ma fyp too :D -jul sux hehehe omg, its u (only old ppl know this reference)
 #master programmer, use what you want. imma just do the functions that i was ASSigned
 
-HOST = ""
-PORT = 
-s = socket.socket()
-s.bind((HOST,PORT))
+#Server IP and Port
+HOST = "127.0.0.1" #Temporary localhost for testing (Make sure to use the client's IP during production
+PORT = 69420
+
+#Start server with the given host and port given and listen for a client
+server = socket.socket()
+server.bind((HOST,PORT))
+server.listen()
+
+#Check whether connection is established
+connection, incAddress = server.accept()
+with connection:
+  print('Connected with: ', incAddress)
+  while True:
+    data = connection.receiver(1024)
+    if not data:
+      break
+     connection.sendall(data)
 
 def cpu_usage():
   
